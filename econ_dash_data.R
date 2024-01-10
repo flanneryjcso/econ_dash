791a78da4bad6a98555c27d4d6d3b44603eff3d7
flanneryjcso <justin.flannery@cso.ie>
2024-01-10 09:52:24 +0000

    Initial upload
    All csv files and econ_dash_data script.

---
+++ econ_dash_data.R
@@ -0,0 +1,21 @@
+library(csodata)
+library(purrr)
+library(readr)
+library(data.table)
+library(stringr)
+
+
+pxstat_codes <- c('NAQ04', 'NQI01', 'GFQ10', 'GFQ12', 'ISQ04', 'BPQ15', 'CPM16', 'TSM01', 'WPM24', 'RSM05', 'NDQ07', 'MSI02', 'TFQ01', 'GFQ01', 'GFQ02', 'TSM09', 'HPM09', 'QLF03',
+                  'MUM01', 'LRM13', 'EHQ03', 'NDQ06', 'MSI02', 'TFQ02', 'TEM01', 'NAQ05')
+
+data <- map(pxstat_codes, cso_get_data, wide_format = FALSE, cache = FALSE)
+names(data) <- map_chr(pxstat_codes, str_to_lower)
+
+
+data %>% names(.) %>%
+  walk(~ write_csv(data[[.]], paste0(., ".csv")))
+
+
+
+
+
